d05f49b52f2c66954b50b1a769f12e4a981c09ec
Ryan Owens <ryowens@siue.edu>
2018-04-19 22:39:57 -0500

    Working on determining bounding box from aruo markers

--- husky_ws/src/husky_marker_tracking/include/aruco_detector.h
+++ husky_ws/src/husky_marker_tracking/include/aruco_detector.h
@@ -1,9 +1,6 @@
-// Aruco libraries
-#include <aruco/aruco.h>
-#include <aruco/cameraparameters.h>
-#include <aruco/cvdrawingutils.h>
-#include <aruco/arucofidmarkers.h>
-
+// Other
+#include <vector>
+#include <string>
 // OpenCV libraries
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
@@ -11,14 +8,32 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
+// Aruco libraries
+#include <opencv2/aruco.hpp>
+// Transforms
+#include <tf/transform_listener.h>
 
 #ifndef ARUCO_DETECTOR_H
 #define ARUCO_DETECTOR_H
 class ArucoDetector {
 public:
-  ArucoDetector(float marker_size, int marker_dictionary_id);
+ArucoDetector(float marker_size, int marker_dictionary_id);
+std::vector<int> markersInView(cv::Mat image);
+std::vector<tf::Transform> getMarkerTransforms(cv::Mat image,
+                                                      cv::Mat CameraMatrix,
+                                                      cv::Mat DistortionMatrix,
+                                                      cv::Size size);
+bool getTransform(const std::string& refFrame,
+                  const std::string& childFrame,
+                  tf::StampedTransform& transform);
 private:
-  float marker_size;
-  int marker_dictionary_id;
+float marker_size;
+int marker_dictionary_id;
+std::string reference_frame = "base_link";
+std::string camera_frame = "axis_base";
+cv::aruco::Dictionary dictionary;
+cv::aruco::DetectorParameters detectorParams;
+tf::TransformListener tfListener_;
+tf::Transform ConvertToTransform(cv::Vec3d rvec, cv::Vec3d tvec);
 };
 #endif
--- husky_ws/src/husky_marker_tracking/src/husky_marker_tracking.cpp
+++ husky_ws/src/husky_marker_tracking/src/husky_marker_tracking.cpp
@@ -12,20 +12,29 @@
 
 // ROI
 #include <sensor_msgs/RegionOfInterest.h>
+#include <sensor_msgs/CameraInfo.h>
 
 // Defines
 #define CONTROLLER_Y_BUTTON 1
 #define CONTROLLER_B_BUTTON 3
 // Globals
 cv::Mat current_image;
+cv::Mat CameraMatrix;
+cv::Size size;
+cv::Mat DistortionMatrix;
 ros::Publisher roi_pub;
 ros::Publisher camera_axis_pub;
 ros::Subscriber controller_sub;
+ros::Subscriber camera_info_sub;
+tf::Transform base_link_transform;
 image_transport::Subscriber image_sub;
 bool enabled = false;
+const float marker_size = 0.0325; // meters
+const int marker_dictionary_id = 0;
 
 void joystickCallback(const sensor_msgs::Joy::ConstPtr& joymsg);
 void imageCallback(const sensor_msgs::ImageConstPtr& imagemsg);
+void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& camInfoMsg);
 
 int main(int argc, char ** argv) {
         ROS_INFO("Waiting for Signal to start marker tracking");
@@ -34,10 +43,15 @@ int main(int argc, char ** argv) {
         ros::Rate loop_rate(2);
         image_transport::ImageTransport transport(nh);
 
+        // Publishers and Subscribers
         roi_pub = nh.advertise<sensor_msgs::RegionOfInterest>("/weederbot/grid", 1);
         camera_axis_pub = nh.advertise<axis_camera::Axis>("/axis/cmd", 1);
+        camera_info_sub = nh.subscribe<sensor_msgs::CameraInfo>("/axis/camerainfo", 10, cameraInfoCallback);
         controller_sub = nh.subscribe<sensor_msgs::Joy>("/joy_teleop/joy", 10, joystickCallback);
         image_sub = transport.subscribe("/axis/image_raw_out", 1, imageCallback);
+
+        // Aruco Detector
+        ArucoDetector detector(marker_size, marker_dictionary_id);
 
         // axis msgs
         axis_camera::Axis axis_msg;
@@ -50,12 +64,31 @@ int main(int argc, char ** argv) {
         camera_axis_pub.publish(axis_msg);
 
         while(ros::ok()) {
+          float length = 0, width = 0;
                 if(enabled) {
-                        //    Check for markers in current view
-                        //    If non in view pan
-                        //    else add to list
-                        //    pan for next marker
-                        //    when done publish
+                  // Get the count of markers in the current camera view
+                  std::vector<int> markers_in_view = detector.markersInView(current_image);
+                  // if the count is greater than zero
+                  if(markers_in_view.size() > 0) {
+                    // Get the poses for the markers
+                    std::vector<tf::Transform> foundPoses = detector.getMarkerTransforms(current_image, CameraMatrix, DistortionMatrix, size);
+                    // Get the distance from the camera to the markers
+                    std::vector<tf::Transform> distances;
+                    for (size_t i = 0; i < foundPoses.size(); i++) {
+                      distances.push_back(foundPoses[i].inverseTimes(base_link_transform));
+                    }
+
+                    if(distances.size() == 1) {
+                      tf::Vector3 a = distances[0].getOrigin();
+                      length = a[0]; // Add Turn Around Length
+                    }else if(distances.size() == 2) {
+                      tf::Vector3 a = distances[0].getOrigin();
+                      length = a[0];
+                      tf::Vector3 b = distances[1].getOrigin();
+                      width = b[0];
+                    }
+                  }
+
                 }
                 ros::spinOnce();
                 loop_rate.sleep();
@@ -80,4 +113,21 @@ void imageCallback(const sensor_msgs::ImageConstPtr& imagemsg) {
                 ROS_ERROR("cv_bridge exception: %s", e.what());
                 return;
         }
+}
+
+void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& camInfoMsg) {
+  CameraMatrix = cv::Mat::zeros(3, 3, CV_64FC1);
+  DistortionMatrix = cv::Mat::zeros(4, 1, CV_64FC1);
+  size = cv::Size(camInfoMsg->height, camInfoMsg->width);
+
+  for (size_t i = 0; i < 9; ++i) {
+    CameraMatrix.at<double>(i%3, i-(i%3)*3) = camInfoMsg->K[i];
+  }
+
+  if(camInfoMsg->D.size() == 4) {
+    for (size_t i = 0; i < 4; ++i) {
+      DistortionMatrix.at<double>(i, 0) = camInfoMsg->D[i];
+    }
+  }
+
 }
